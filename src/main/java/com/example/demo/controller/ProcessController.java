810a8f8368e9484c84ffacdb7a798c3956cbbccc
ashokmaram1990 <ashokmaram1990@gmail.com>
2020-06-09 16:17:29 +0530

    added port

--- src/main/java/com/example/demo/controller/ProcessController.java
+++ src/main/java/com/example/demo/controller/ProcessController.java
@@ -1,14 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Configuration
+@Component
 @RequestMapping("/process")
 public class ProcessController {
 	
+	@RequestMapping("/add")
 	public String getDetails() {
-		return "hai Student";
+		return "HI All welcome to DevOps Practice";
 	}
 
 }
